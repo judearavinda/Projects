0cd06f8ef42b2328b480c01646bcdae2790cc8a3
judearavinda <judecanada911@hotmail.com>
2015-05-19 22:05:04 -0400

    Commiting WebBrowser

---
+++ WebBrowser/web_browser.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.SearchButton = New System.Windows.Forms.Button()
+        Me.HomeButton = New System.Windows.Forms.Button()
+        Me.RefreshButton = New System.Windows.Forms.Button()
+        Me.ForwardButton = New System.Windows.Forms.Button()
+        Me.BackButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(23, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(660, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(23, 89)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(726, 224)
+        Me.WebBrowser1.TabIndex = 1
+        '
+        'SearchButton
+        '
+        Me.SearchButton.Location = New System.Drawing.Point(689, 25)
+        Me.SearchButton.Name = "SearchButton"
+        Me.SearchButton.Size = New System.Drawing.Size(60, 20)
+        Me.SearchButton.TabIndex = 2
+        Me.SearchButton.Text = "Go"
+        Me.SearchButton.UseVisualStyleBackColor = True
+        '
+        'HomeButton
+        '
+        Me.HomeButton.Location = New System.Drawing.Point(315, 60)
+        Me.HomeButton.Name = "HomeButton"
+        Me.HomeButton.Size = New System.Drawing.Size(75, 23)
+        Me.HomeButton.TabIndex = 3
+        Me.HomeButton.Text = "Home"
+        Me.HomeButton.UseVisualStyleBackColor = True
+        '
+        'RefreshButton
+        '
+        Me.RefreshButton.Location = New System.Drawing.Point(217, 60)
+        Me.RefreshButton.Name = "RefreshButton"
+        Me.RefreshButton.Size = New System.Drawing.Size(75, 23)
+        Me.RefreshButton.TabIndex = 4
+        Me.RefreshButton.Text = "Refresh"
+        Me.RefreshButton.UseVisualStyleBackColor = True
+        '
+        'ForwardButton
+        '
+        Me.ForwardButton.Location = New System.Drawing.Point(120, 60)
+        Me.ForwardButton.Name = "ForwardButton"
+        Me.ForwardButton.Size = New System.Drawing.Size(75, 23)
+        Me.ForwardButton.TabIndex = 5
+        Me.ForwardButton.Text = "Forward"
+        Me.ForwardButton.UseVisualStyleBackColor = True
+        '
+        'BackButton
+        '
+        Me.BackButton.Location = New System.Drawing.Point(23, 60)
+        Me.BackButton.Name = "BackButton"
+        Me.BackButton.Size = New System.Drawing.Size(75, 23)
+        Me.BackButton.TabIndex = 6
+        Me.BackButton.Text = "Back"
+        Me.BackButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(796, 334)
+        Me.Controls.Add(Me.BackButton)
+        Me.Controls.Add(Me.ForwardButton)
+        Me.Controls.Add(Me.RefreshButton)
+        Me.Controls.Add(Me.HomeButton)
+        Me.Controls.Add(Me.SearchButton)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents SearchButton As System.Windows.Forms.Button
+    Friend WithEvents HomeButton As System.Windows.Forms.Button
+    Friend WithEvents RefreshButton As System.Windows.Forms.Button
+    Friend WithEvents ForwardButton As System.Windows.Forms.Button
+    Friend WithEvents BackButton As System.Windows.Forms.Button
+
+End Class
---
+++ WebBrowser/web_browser.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SearchButton.Click
+        WebBrowser1.Navigate(TextBox1.Text)
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Text = "Web Browser"
+    End Sub
+
+    Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles HomeButton.Click
+        WebBrowser1.GoHome()
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BackButton.Click
+        WebBrowser1.GoBack()
+    End Sub
+
+    Private Sub ForwardButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ForwardButton.Click
+        WebBrowser1.GoForward()
+    End Sub
+
+    Private Sub RefreshButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RefreshButton.Click
+        WebBrowser1.Refresh()
+    End Sub
+     
+End Class
